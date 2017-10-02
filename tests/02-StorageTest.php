ba4d16f18e3ddefb87b1f853a879c35888d95d1c
darkmist <me@donbstringham.us>
2017-10-02 10:35:47 -0600

    Update 02-StorageTest.php
    Added more unit-tests.

--- tests/02-StorageTest.php
+++ tests/02-StorageTest.php
@@ -1,9 +1,7 @@
 <?php
-
 require_once(__DIR__."/../src/02-DBMockStorage.php");
 require_once(__DIR__."/../src/02-FileStorage.php");
 require_once(__DIR__."/../src/02-MemStorage.php");
-
 class StorageTest extends \PHPUnit\Framework\TestCase {
   public function testDBMockStorage00() {
     // arrange
@@ -14,7 +12,6 @@ class StorageTest extends \PHPUnit\Framework\TestCase {
     $this->assertEquals("MySQL", $actual);
     $this->assertEquals(1, $harness->Count());
   }
-
   public function testDBMockStorage01() {
     // arrange
     $harness = new DBMockStorage("MySQL", "item0");
@@ -24,7 +21,6 @@ class StorageTest extends \PHPUnit\Framework\TestCase {
     // assert
     $this->assertEquals(2, $harness->Count());
   }
-
  public function testFileStorage00() {
     // arrange
     $harness = new FileStorage();
@@ -34,7 +30,6 @@ class StorageTest extends \PHPUnit\Framework\TestCase {
     // assert
     $this->assertEquals(5, $actual);
   }
-
  public function testFileStorage01() {
     // arrange
     $harness = new FileStorage();
@@ -45,7 +40,29 @@ class StorageTest extends \PHPUnit\Framework\TestCase {
     // assert
     $this->assertEquals(10, $actual);
   }
-
+  public function testFileStorage02()
+  {
+      // arrange
+      $writer = new FileStorage();
+      $writer->Put("item0");
+      $writer->Put("item1");
+      $reader = new FileStorage(); // Don't use an instance counter.
+      // act
+      $actual = $reader->Count();
+      // assert
+      $this->assertEquals(10, $actual);
+  }
+  public function testFileStorage03()
+  {
+      // arrange
+      $harness = new FileStorage();
+      $harness->Put("one");
+      $harness->Put("four");
+      // act
+      $actual = $harness->Count();
+      // assert
+      $this->assertEquals(7, $actual);
+  }
   public function testMemStorage00() {
     // arrange
     $harness = new MemStorage("item0");
@@ -54,7 +71,6 @@ class StorageTest extends \PHPUnit\Framework\TestCase {
     // assert
     $this->assertEquals(1, $actual);
   }
-
   public function testMemStorage01() {
     // arrange
     $harness = new MemStorage("item0");
@@ -64,7 +80,6 @@ class StorageTest extends \PHPUnit\Framework\TestCase {
     // assert
     $this->assertEquals(2, $actual);
   }
-
   protected function tearDown() {
     system("rm -f data.txt");
   }
